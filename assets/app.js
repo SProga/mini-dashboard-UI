3ba57c7c637aae7090b1d4bd3dced5c1d1d8b63e
SProga <shane.306@hotmail.com>
2021-04-28 19:17:50 -0400

    first commit

---
+++ assets/app.js
@@ -0,0 +1,25 @@
+let btn = document.querySelector(".nav-toggler");
+let icon = document.querySelector(".nav-btn");
+let menu = document.querySelector(".menu");
+let category = document.querySelectorAll(".category");
+let style = null;
+
+btn.addEventListener("click", () => {
+  menu.classList.toggle("toggle-menu");
+  for (let i = 0; i < menu.children.length; i++) {
+    if (!menu.children[i].classList.contains("nav-toggler"))
+      menu.children[i].classList.toggle("toggle-menu-content");
+  }
+  icon.classList.toggle("rotate");
+});
+
+category.forEach((cat) => {
+  cat.addEventListener("mouseenter", () => {
+    style = getComputedStyle(cat);
+    let background = style.getPropertyValue("background-color");
+    cat.parentElement.parentElement.style.boxShadow = `0px 3px 4px ${background}`;
+  });
+  cat.addEventListener("mouseleave", () => {
+    cat.parentElement.parentElement.style.boxShadow = "unset";
+  });
+});
